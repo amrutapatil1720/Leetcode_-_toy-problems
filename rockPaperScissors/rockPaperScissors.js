5771928dd245412bccd701ed7fbda86722cd4198
Amruta Patil <amrutapatil1720@gmail.com>
2019-10-29 09:48:24 -0700

    toy1

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,32 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
-  // TODO: your solution here
-};
+var rockPaperScissors = function (vlue) {
+  // TODO: your solution 
+  var array=[];
+  var player=['RRRRR', 'RRRRP', 'RRRRS'];
 
+  for(var i=0;i<player.length;i++)
+    for(var j=0;j<player.length;j++)
+      for(var k=0;k<player.length;k++)
+             array.push(player[k]);
+             array.push(player[j]);
+             array.push(player[i]);
+
+
+
+             return array;
+  
+};
+//console.log(rockPaperScissors(5));
+
+//another way 
+
+// var rockPaperScissors = function (vlue) {
+// var array=[];
+// var player=['RRRRR', 'RRRRP', 'RRRRS'];
+//read random element from player array at random index value and push it to resulting arrray
+//var ran= player[Math.floor(Math.random() * player.length)];
+//array.push(ran);
+//return array;
+// }
