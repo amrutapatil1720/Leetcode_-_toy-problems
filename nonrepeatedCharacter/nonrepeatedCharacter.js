1e24c7007e661d63b2725cfd153efa9d6cf3cbac
Amruta Patil <amrutapatil1720@gmail.com>
2019-11-12 09:43:00 -0800

    first nonrepeated character in string

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,19 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  var obj = {};
+
+  for (var i = 0; i < string.length; i++) {
+    if (obj[string[i]] === undefined) {
+      obj[string[i]] = true;
+    } else {
+      obj[string[i]] = false;
+    }
+  }
+
+  var i = 0;
+  while (obj[string[i]] === false) {
+    i++;
+  }
+  return string[i];
 };
